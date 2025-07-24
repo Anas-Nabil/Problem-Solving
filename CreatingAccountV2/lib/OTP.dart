feed82c1ce0a609e3d6bb1012cd94e6635af901a
Anas-Nabil <anas.awadallah@ejust.edu.eg>
2025-07-24 04:19:21 +0300

    Add new folder for my work

---
+++ CreatingAccountV2/lib/OTP.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:taskk/OTP_Content.dart';
+import 'package:taskk/app_colors.dart';
+
+
+
+class otpScreen extends StatelessWidget {
+  const otpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.whiteApp,
+
+      body: Align(
+        alignment: Alignment.bottomCenter,
+
+        child: Container(
+          height: 600,
+          decoration: BoxDecoration(
+            color: AppColors.mainColor,
+            borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+          ),
+          child: otpScreenContent(),
+        ),
+      ),
+    );
+  }
+}
